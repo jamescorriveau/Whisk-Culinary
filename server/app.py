83f73afba3dc0430aab7a8c21530f06b17351c2f
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-01-29 23:15:14 -0500

    added routes for testing

--- server/app.py
+++ server/app.py
@@ -1,12 +1,26 @@
 # app.py
 
-from flask import Flask, send_from_directory
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-@app.route('/product/<filename>')
-def serve_product_image(filename):
-    return send_from_directory('product_images', filename)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from models import Product
+
+@app.route('/products')
+def get_products():
+    products = Product.query.all()
+    return jsonify([product.to_dict() for product in products])
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
