0861c03e135aa05239aeca78565f48bfef3c6b98
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-01-29 23:16:21 -0500

    added models

---
+++ server/models.py
@@ -0,0 +1,19 @@
+# models.py
+
+from app import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    image = db.Column(db.String(200))
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": self.price,
+            "quantity": self.quantity
+        }
