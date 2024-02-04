3e31724d5edba8922374127ec04e16a49f980602
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-02-04 02:22:38 -0500

    added images and carousel to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,28 @@
 // Home.jsx
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import globalKnivesImage from "../images/global-knives-header-banner-1564754806.jpeg";
+import kitchenAidImage from "../images/kitchenaid-brand-header-banner-1564126621.webp";
+import staubMockupImage from "../images/staubmockupbanner1.1.jpeg";
+import "../App.css";
 
 function Home() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [globalKnivesImage, kitchenAidImage, staubMockupImage];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <div>
-      <h1></h1>
-      <p></p>
+      <div className="image-slider">
+        <img src={images[currentImageIndex]} alt="Product" />
+      </div>
     </div>
   );
 }
