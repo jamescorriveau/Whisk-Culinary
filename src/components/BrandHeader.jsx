6569bd45352376690f1c5b3934ec890ff1fc2315
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-02-04 16:27:07 -0500

    align logo positioning left/right in BrandHeader

--- src/components/BrandHeader.jsx
+++ src/components/BrandHeader.jsx
@@ -5,7 +5,9 @@ import React from "react";
 function BrandHeader() {
   return (
     <div className="bg-white text-black py-7 px-4 pt-7 fixed top-0 left-0 w-full z-50">
-      <div className="flex items-center justify-between max-w-screen-lg mx-auto w-full">
+      {/* Full width container */}
+      <div className="flex justify-between items-center w-full px-4">
+        {/* Whisk Logo aligned to the left */}
         <div className="flex items-center">
           <img
             src="/Whisk-logo.png"
@@ -13,6 +15,8 @@ function BrandHeader() {
             style={{ paddingRight: "20px" }}
           />
         </div>
+
+        {/* Centered Brand Name and Tagline */}
         <div className="flex-1 text-center">
           <h1
             className="text-5xl font-bold mb-2"
@@ -24,6 +28,8 @@ function BrandHeader() {
             Precision Cooking at Home
           </p>
         </div>
+
+        {/* Partner Logos aligned to the right */}
         <div className="flex items-center">
           <img
             src="/Global-logo.png"
