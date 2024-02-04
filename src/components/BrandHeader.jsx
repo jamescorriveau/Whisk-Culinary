f04a6caa954a0d2b8081fdb7748b46019658028d
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-02-04 16:17:26 -0500

    positioning of BrandHeader logos

--- src/components/BrandHeader.jsx
+++ src/components/BrandHeader.jsx
@@ -5,26 +5,26 @@ import React from "react";
 function BrandHeader() {
   return (
     <div className="bg-white text-black py-7 px-4 pt-7 fixed top-0 left-0 w-full z-50">
-      <div className="flex justify-between items-center max-w-screen-lg mx-auto">
+      <div className="flex items-center justify-between max-w-screen-lg mx-auto w-full">
         <div className="flex items-center">
           <img
             src="/Whisk-logo.png"
             alt="Whisk Logo"
             style={{ paddingRight: "20px" }}
           />
-          <div className="text-center">
-            <h1
-              className="text-5xl font-bold mb-2"
-              style={{ fontFamily: "Didot, serif" }}
-            >
-              Whisk
-            </h1>
-            <p className="text-3xl" style={{ fontFamily: "Didot, serif" }}>
-              Precision Cooking at Home
-            </p>
-          </div>
         </div>
-        <div className="logo flex">
+        <div className="flex-1 text-center">
+          <h1
+            className="text-5xl font-bold mb-2"
+            style={{ fontFamily: "Didot, serif" }}
+          >
+            Whisk
+          </h1>
+          <p className="text-3xl" style={{ fontFamily: "Didot, serif" }}>
+            Precision Cooking at Home
+          </p>
+        </div>
+        <div className="flex items-center">
           <img
             src="/Global-logo.png"
             alt="Global Logo"
