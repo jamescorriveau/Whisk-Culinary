ec41ff5020549a2c1347c3d62bbfd157c8b7fb6a
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-02-04 22:06:03 -0500

    added styling to the ShoppingCart buttons

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -7,47 +7,56 @@ import ProductImageComponent from "./ProductImageComponent";
 function ShoppingCart() {
   const { cart, addToCart, removeFromCart } = useContext(CartContext);
 
-  const incrementQuantity = (productId) => {
+  const handleQuantityChange = (productId, newQuantity) => {
     const product = cart.find((item) => item.id === productId);
-    addToCart(product, 1);
-  };
-
-  const decrementQuantity = (productId) => {
-    const product = cart.find((item) => item.id === productId);
-    if (product.quantity > 1) {
-      addToCart(product, -1);
-    } else {
-      removeFromCart(productId);
-    }
+    const quantityDifference = newQuantity - product.quantity;
+    addToCart(product, quantityDifference);
   };
 
   return (
-    <div style={{ paddingTop: "20px" }}>
+    <div className="pt-5">
       {cart.length > 0 ? (
         <ul>
           {cart.map((item, index) => (
-            <li
-              key={index}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginBottom: "10px",
-              }}
-            >
+            <li key={index} className="flex items-center mb-2.5">
               <ProductImageComponent
                 imageUrl={item.image}
                 altText={item.name}
                 imageSizeClass="w-32 h-32"
               />
-              <div style={{ marginLeft: "10px" }}>
-                <strong>{item.name}</strong> - Quantity: {item.quantity} <br />
-                <strong></strong> ${item.price}
-                <div>
-                  <button onClick={() => incrementQuantity(item.id)}>+</button>
-                  <button onClick={() => decrementQuantity(item.id)}>-</button>
-                  <button onClick={() => removeFromCart(item.id)}>
-                    Remove
-                  </button>
+              <div className="ml-2.5 flex-grow">
+                <strong>{item.name}</strong>
+                <div className="flex justify-between items-center mt-2">
+                  <div>
+                    <strong>Price:</strong> ${item.price}
+                  </div>
+                  <div>
+                    <div className="inline-flex items-center bg-dark-gold text-white px-3 py-1.5 rounded text-sm cursor-pointer mr-2">
+                      <span>Qty:</span>
+                      <select
+                        value={item.quantity}
+                        onChange={(e) =>
+                          handleQuantityChange(
+                            item.id,
+                            parseInt(e.target.value)
+                          )
+                        }
+                        className="bg-transparent text-white ml-1 focus:outline-none appearance-none cursor-pointer"
+                      >
+                        {[...Array(10).keys()].map((num) => (
+                          <option key={num + 1} value={num + 1}>
+                            {num + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <button
+                      onClick={() => removeFromCart(item.id)}
+                      className="bg-red-500 text-white px-3 py-1.5 rounded text-sm focus:outline-none focus:ring"
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
               </div>
             </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gold": "#d5b500",
+      },
+    },
   },
   plugins: [],
 };
