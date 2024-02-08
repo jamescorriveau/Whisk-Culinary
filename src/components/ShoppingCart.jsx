260258527eaf476a791118ed042627e8a7e0a9fe
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-02-08 16:49:59 -0500

    added thank you purchase alert

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,28 +1,40 @@
 // ShoppingCart.jsx
 
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "./CartContext";
 import ProductImageComponent from "./ProductImageComponent";
 import { PayPalButtons } from "@paypal/react-paypal-js";
 
 function ShoppingCart() {
-  const { cart, addToCart, removeFromCart } = useContext(CartContext);
+  const { cart, setCart, removeFromCart } = useContext(CartContext);
+  const [cartItems, setCartItems] = useState([]);
 
-  const handleQuantityChange = (productId, newQuantity) => {
-    const product = cart.find((item) => item.id === productId);
-    const quantityDifference = newQuantity - product.quantity;
-    addToCart(product, quantityDifference);
+  // Fetch the detailed cart information
+  useEffect(() => {
+    setCartItems(cart);
+  }, [cart]); // React to changes in the cart
+
+  const handleRemoveFromCart = (productId) => {
+    removeFromCart(productId);
   };
 
-  const totalPrice = cart.reduce((total, item) => {
-    return total + item.price * item.quantity;
-  }, 0);
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 
+  // Function to clear the cart after successful payment
+  const clearCart = () => {
+    setCart([]);
+    setCartItems([]);
+  };
+
+  // Render the shopping cart
   return (
     <div className="pt-5 px-4">
-      {cart.length > 0 ? (
+      {cartItems.length > 0 ? (
         <ul>
-          {cart.map((item, index) => (
+          {cartItems.map((item, index) => (
             <li
               key={index}
               className="flex items-center mb-2.5 py-2 border-b border-gray-300"
@@ -36,30 +48,11 @@ function ShoppingCart() {
                 <strong>{item.name}</strong>
                 <div className="flex justify-between items-center mt-2">
                   <div>
-                    <strong>Price:</strong> ${item.price}
+                    <strong>Price:</strong> ${item.price.toFixed(2)}
                   </div>
                   <div>
-                    <div className="inline-flex items-center bg-black text-dark-gold px-3 py-1.5 rounded text-sm cursor-pointer mr-2">
-                      <span>Qty:</span>
-                      <select
-                        value={item.quantity}
-                        onChange={(e) =>
-                          handleQuantityChange(
-                            item.id,
-                            parseInt(e.target.value)
-                          )
-                        }
-                        className="bg-transparent text-dark-gold ml-1 focus:outline-none appearance-none cursor-pointer"
-                      >
-                        {[...Array(10).keys()].map((num) => (
-                          <option key={num + 1} value={num + 1}>
-                            {num + 1}
-                          </option>
-                        ))}
-                      </select>
-                    </div>
                     <button
-                      onClick={() => removeFromCart(item.id)}
+                      onClick={() => handleRemoveFromCart(item.id)}
                       className="bg-red-500 text-white px-3 py-1.5 rounded text-sm focus:outline-none focus:ring"
                     >
                       Remove
@@ -73,7 +66,7 @@ function ShoppingCart() {
       ) : (
         <p>Your Shopping Bag is empty.</p>
       )}
-      {cart.length > 0 && (
+      {cartItems.length > 0 && (
         <div className="flex justify-end mt-2.5 mb-4">
           <div className="flex items-center">
             <div className="mr-4">
@@ -94,10 +87,11 @@ function ShoppingCart() {
               }}
               onApprove={(data, actions) => {
                 return actions.order.capture().then((details) => {
+                  alert("Thank you for your purchase!");
+                  clearCart();
                   console.log(
                     "Transaction completed by " + details.payer.name.given_name
                   );
-                  // add logic after successful payment
                 });
               }}
             />
